98f394af9748ec9e79b15283604768c6c223ed80
jaquis mccullough <mccullough.jaquis@gmail.com>
2018-01-03 08:32:35 -0500

    first commit

---
+++ my_first_program.rb
@@ -0,0 +1,205 @@
+# puts "Hello! Welcome to my first program!"
+# print "What's your name?"
+# name = gets.chomp 
+
+# puts "Hello, #{name}! Nice to meet you!"
+# puts "Hello, " + name + "! Nice to meet you."
+
+# puts "Enter cat or dog"
+# animal = gets.chomp.downcase
+# if animal =="dog"
+# 	puts "woof!"
+# else
+# 	puts "meow!"
+# end
+
+# num 1 = 7
+# puts "Give me a number between 1-10"
+# num 2= gets.chomp.to_i
+# if num 2==num1
+# 	puts "wow"
+# else
+# 	puts "nope"
+# end
+
+# puts "What is your grade?"
+# grade= gets.chomp.to_i
+# if grade >= 60
+# 	puts "you passed"
+# else
+# 	puts "you have to retake the class"
+# end		
+
+# puts "Enter an animal?"
+# animal = gets.chomp.downcase
+# if animal =="dog"
+# 	puts "woof!"
+# elsif animal == "cat"
+# 	puts "meow!"
+# elsif animal == "horse"
+# 	puts "neigh"
+# elsif animal == "cow"
+# 	puts "moo"	
+
+# else 
+# 	puts "I don't know my animal sound."
+# end
+
+# num = 30
+# puts "Pick a number between 1 and 100"
+# user_num = gets.chomp.to_i
+
+# if user_num == num
+# 	puts "You must be psychic!"
+# elsif user_num <= num + 5 && user_num>= num - 5
+# 	puts "Ooh, so close the number is 30."	
+# else
+# 	puts "Nope, you are way off"	
+# end	
+
+# puts "Enter an animal?"
+# animal = gets.chomp.downcase
+# case animal =="dog"
+# 	when "dog" then  puts "woof!"
+# 	when "cat" then puts "meow!"
+# 	when "horse" then puts "neigh"
+# 	when "cow" then puts "moo"	
+# else 
+# 	puts "I don't know my animal sound."
+# end
+
+
+# puts "What is your favorite crayon?"
+# crayon = gets.chomp.reverse.upcase 
+# puts crayon
+
+# puts "How are you feeling today?"
+# feeling = gets.chomp.downcase
+# puts feeling.length
+# puts "meow #{feeling}"
+
+# num1 = 7
+# num2 = 9
+# puts num1 += num2
+# puts num1 -= num2
+# puts num1 *= num2
+# puts num1 /= num2
+
+# puts "What is your favorite sentence?"
+# favorite = gets.chomp
+# puts "What is your favorite word in that sentence?"
+# word = gets.chomp  
+# puts favorite.index(word)
+
+# puts "What is the cost of my dinner?"
+# num = gets.chomp.to_f
+# puts num * 1.18
+
+# puts "How old are you?"
+# num = gets.chomp.to_i
+# puts (num * 17356800)
+
+# puts "How old are you?"
+# num = gets.chomp.to_i
+# puts (num * 0.665)
+
+
+
+puts "Yesterday, we were #{verb} pizza when I realized that we didn't have any dessert to follow our dinner. I ordered a dozen #{pluralnoun} that had #{adjective} sprinkles. Afterward, #{preposition} were going to have coffee by the #{geographicalfeature} but it was too cold. We went home and added a few #{object} to the fire, and watched episode #{number} of game of thrones."
+puts "#{verb}"
+
+puts "#{verb}"
+	verb = gets.chomp
+puts "#{pluralnoun}"
+	pluralnoun = gets.chomp
+puts "#{adjective}"
+ adjective = gets.chomp
+puts "#{geographicalfeature}"
+ geographicalfeature = gets.chomp
+puts "#{object}"
+ object = gets.chomp
+puts "#{number}"
+	number = gets.chomp
+
+
+
+
+
+ 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
